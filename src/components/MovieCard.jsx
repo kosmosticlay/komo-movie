5782037962b3b7d16b878ff284938b6756d295b9
kosmosticlay <kosmosticlay@gmail.com>
2024-11-06 19:00:25 +0900

    feat/style: 슬라이드 기능 추가(Swiper)
    - Swiper 라이브러리 추가
    - MovieCardList 컴포넌트에 Swiper 적용
      - showPagination prop값에 따라 Pagination기능을 동적으로 활성화
      - swiper 관련 CSS 클래스에 커스텀 스타일 적용
        (CSS 우선순위 적용을 위해 !important 추가)

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function MovieCard({ id, poster, title, rating }) {
   return (
-    <li className="w-56 h-80 border-4 border-black rounded-xl">
-      <Link to={"/details"} className="">
-        <img className="w-full h-4/5 overflow-hidden rounded-xl" src={poster} />
+    <li className="mx-2 h-64 rounded-xl overflow-hidden bg-gray-200">
+      <Link
+        to={"/details"}
+        className="flex flex-col items-center overflow-hidden "
+      >
+        <img className="w-full h-4/5" src={poster} />
 
         <h2 className="movie-title">{title}</h2>
         <p className="movie-rating">평점: {rating}</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,11 @@
-import { useState } from "react";
-import MovieCard from "../components/MovieCard";
-import movieListData from "../data/movieListData.json";
-import { BASE_URL } from "../App";
+import MovieCardList from "../components/MovieCardList";
 
 export default function Home() {
-  const json = movieListData;
-  const data = json.results;
-  const [movies, setMovies] = useState(data);
-
   //console.log(movies);
   return (
     <>
       <h1 className="text-center text-3xl font-bold">Home 컴포넌트</h1>
-      <ul className="flex flex-wrap gap-10 justify-center">
-        {movies.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            id={movie.id}
-            poster={`${BASE_URL}${movie.backdrop_path}`}
-            title={movie.title}
-            rating={movie.rating}
-          />
-        ))}
-      </ul>
+      <MovieCardList showPagination={true} />
     </>
   );
 }
