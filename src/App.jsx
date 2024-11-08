3c72096d88fb18b293f47c3c1c932519d61cc70b
kosmosticlay <kosmosticlay@gmail.com>
2024-11-08 10:16:23 +0900

    feat: NavBar 컴포넌트 기본 레이아웃 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import MovieDetail from "./pages/MovieDetail";
 import Home from "./pages/Home";
+import NavBar from "./components/layout/NavBar";
 
 export const BASE_URL = "https://image.tmdb.org/t/p/w500";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/details/:id" element={<MovieDetail />} />
---
+++ src/components/layout/NavBar.jsx
@@ -0,0 +1,28 @@
+import {
+  Bars3Icon,
+  ArrowLeftEndOnRectangleIcon as LoginIcon,
+} from "@heroicons/react/24/outline";
+import { UserPlusIcon as SignUpIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <nav className="fixed top-0 z-10 flex flex-col items-center w-20 h-screen py-10 text-xl font-bold text-white nav-wide bg-slate-600">
+      <Bars3Icon className="w-10 h-10 text-black stroke-hover md:hidden" />
+      <div className="flex flex-col items-center justify-between h-full md:flex-row ">
+        <h1 className="hidden md:block">logo</h1>
+        <ul>{/* 홈, 즐겨찾기등 */}</ul>
+        <section className="*:ml-5 md:flex ">
+          <Link to="/login">
+            <p className="hidden md:block">Login</p>
+            <LoginIcon className="w-10 h-10 stroke-hover md:hidden" />
+          </Link>
+          <Link to="/sign-up">
+            <p className="hidden md:block">Sign Up</p>
+            <SignUpIcon className="w-10 h-10 stroke-hover md:hidden" />
+          </Link>
+        </section>
+      </div>
+    </nav>
+  );
+}
